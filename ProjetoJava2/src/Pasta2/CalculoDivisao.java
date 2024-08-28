b1b3d74eafa1f79f5ed0c06d7369503b790192a1
Edeilson Gonçalves <edeilsoniasd@outlook.com>
2024-08-28 10:21:57 -0300

    Revert "Mais um comentário"
    This reverts commit a8d0490abdb8ca06605c776031227f69c49f53fd.

--- ProjetoJava2/src/Pasta2/CalculoDivisao.java
+++ ProjetoJava2/src/Pasta2/CalculoDivisao.java
@@ -4,7 +4,6 @@ public class CalculoDivisao {
 
 	public static void main(String[] args) {
 		// Gerando um calculo matemático de divisão
-		// Através da variável calculoDivisao
 		int calculoDivisao = 10 / 2;
 		System.out.println(calculoDivisao);
 
