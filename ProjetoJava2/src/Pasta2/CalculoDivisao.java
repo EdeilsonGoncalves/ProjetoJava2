a8d0490abdb8ca06605c776031227f69c49f53fd
Edeilson Gonçalves <edeilsoniasd@outlook.com>
2024-08-28 10:18:59 -0300

    Mais um comentário

--- ProjetoJava2/src/Pasta2/CalculoDivisao.java
+++ ProjetoJava2/src/Pasta2/CalculoDivisao.java
@@ -4,6 +4,7 @@ public class CalculoDivisao {
 
 	public static void main(String[] args) {
 		// Gerando um calculo matemático de divisão
+		// Através da variável calculoDivisao
 		int calculoDivisao = 10 / 2;
 		System.out.println(calculoDivisao);
 
