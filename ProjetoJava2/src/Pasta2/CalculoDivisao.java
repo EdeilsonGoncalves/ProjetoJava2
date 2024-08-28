386b7f29c0df81bf97cf4b65601f0624eccb402e
Edeilson Gonçalves <edeilsoniasd@outlook.com>
2024-08-28 09:52:30 -0300

    Alteração no comentário

--- ProjetoJava2/src/Pasta2/CalculoDivisao.java
+++ ProjetoJava2/src/Pasta2/CalculoDivisao.java
@@ -3,7 +3,7 @@ package Pasta2;
 public class CalculoDivisao {
 
 	public static void main(String[] args) {
-		// Gerando um calculo de divisão
+		// Gerando um calculo matemático de divisão
 		int calculoDivisao = 10 / 2;
 		System.out.println(calculoDivisao);
 
